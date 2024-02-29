233b69ee855c26bbecf086422614678ac4b0c385
Szymoek02 <stajszczykszymon@gmail.com>
2024-02-29 23:30:44 +0000

    Update Obiekty wewnątrz obiektu.cpp

--- C++/Poziom rozszerzony/Struktury danych/Obiekty wewnątrz obiektu.cpp
+++ C++/Poziom rozszerzony/Struktury danych/Obiekty wewnątrz obiektu.cpp
@@ -1,15 +1,36 @@
-// You can do this!
+#include <Node.hpp>
 
-// Code here...
-#include "Node.hpp"
-bool find(const Node* root, int n)
+bool find(const Node* node, int n)
 {
-    if(!root) return false;
-    if(root->value == n) return true;
-    return find(root->left, n) || find(root->right, n);
+    if(node == nullptr)
+        return false;
+
+    if(node->value == n)
+        return true;
+
+    if(n < node->value)
+        return find(node->left, n);
+    else
+        return find(node->right, n);
 }
 
-void emplace(Node* root, int n)
+void emplace(Node* node, int value)
 {
+    if(node == nullptr)
+        return;
 
+    if(value < node->value)
+    {
+        if(node->left == nullptr)
+            node->left = new Node{ value };
+        else
+            emplace(node->left, value);
+    }
+    else if(value > node->value)
+    {
+        if(node->right == nullptr)
+            node->right = new Node{ value };
+        else
+            emplace(node->right, value);
+    }
 }
