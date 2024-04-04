db7686701e4524c5c82dbdccd3735eec62b17009
Szymoek02 <stajszczykszymon@gmail.com>
2024-04-04 14:51:29 +0200

    Update 16. Ćwiczenie - zaprzęg św. Mikołaja.py

--- Python/Poziom Rozszerzony/Zaawansowane funkcje funkcji/16. Ćwiczenie - zaprzęg św. Mikołaja.py
+++ Python/Poziom Rozszerzony/Zaawansowane funkcje funkcji/16. Ćwiczenie - zaprzęg św. Mikołaja.py
@@ -2,7 +2,7 @@ def setHarness(minRP, **reniferowie):
     sorted_reniferowie = sorted(reniferowie, key=reniferowie.get, reverse=True)
     total_power = 0
     selected_reniferowie = []
-    s = ['Dasher', 'Dancer', 'Prancer', 'Vixen', 'Comet', 'Cupid', 'Donner', 'Blitzen', 'Rudolph']
+    s = {'Dasher', 'Dancer', 'Prancer', 'Vixen', 'Comet', 'Cupid', 'Donner', 'Blitzen', 'Rudolph'}
 
     for renifer in sorted_reniferowie:
         if renifer in s:
