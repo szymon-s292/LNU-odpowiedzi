3551e569afcfff926f76e31318d2d36f426b21b5
Szymoek02 <stajszczykszymon@gmail.com>
2024-04-04 20:12:47 +0200

    Update Ćwiczenie odchylenie.py

--- Python/Poziom Rozszerzony/Zaawansowane napisy/Ćwiczenie odchylenie.py
+++ Python/Poziom Rozszerzony/Zaawansowane napisy/Ćwiczenie odchylenie.py
@@ -1,6 +1,10 @@
-def deviation(readings):
-    mean = sum(readings) / len(readings)
-    for reading in readings:
-        deviation = reading - mean
-        deviation_str = '{:.14f}'.format(deviation)
-        print(f"{'o'*(24-len(deviation_str))}{deviation_str}")
+def deviation(readings):
+    mean = sum(readings) / len(readings)
+    for reading in readings:
+        dev = (reading - mean)
+        deviation_str = str(dev)
+        if dev >= 0:
+            print(f"{'o'*(25-len(deviation_str))}{deviation_str}")
+        else:
+            deviation_str = str(abs(dev))
+            print(f"{'-' + 'o'*(24-len(deviation_str))}{deviation_str}")
