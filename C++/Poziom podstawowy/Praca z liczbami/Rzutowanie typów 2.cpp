bbcc692d154116d322aa6d51bc626b684b430f0c
szymon-s292 <stajszczykszymon@gmail.com>
2024-05-13 21:42:18 +0200

    Update Rzutowanie typów 2.cpp

--- C++/Poziom podstawowy/Praca z liczbami/Rzutowanie typów 2.cpp
+++ C++/Poziom podstawowy/Praca z liczbami/Rzutowanie typów 2.cpp
@@ -1,10 +1,9 @@
 int Round( double value )
 {
     int n = (int)value;
-    if(n == value)
-        return n;
     if(n + 0.5 <= value && value > 0)
         return n + 1;
-    if(n - 0.5 >= value && value < 0)
+    else if(n - 0.5 >= value && value < 0)
         return n - 1;
+    return n;
 }
