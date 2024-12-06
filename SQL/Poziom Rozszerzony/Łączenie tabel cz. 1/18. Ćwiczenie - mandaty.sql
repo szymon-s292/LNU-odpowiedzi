a7c744de94887093e0329d0c71f9956e6944c324
Zeroks <85626218+Zeroks1@users.noreply.github.com>
2024-12-06 22:27:38 +0100

    Update 18. Ćwiczenie - mandaty.sql
    poprawa błędu

--- SQL/Poziom Rozszerzony/Łączenie tabel cz. 1/18. Ćwiczenie - mandaty.sql
+++ SQL/Poziom Rozszerzony/Łączenie tabel cz. 1/18. Ćwiczenie - mandaty.sql
@@ -1,1 +1,1 @@
-INNER JOIN drivers ON tickets.driver_id = drivers.id;
+SELECT t.ticket_nr, d.first_name, d.last_name, v.model FROM tickets t JOIN drivers d ON t.driver_id = d.id JOIN vehicles v ON t.licence_plate = v.licence_plate;
