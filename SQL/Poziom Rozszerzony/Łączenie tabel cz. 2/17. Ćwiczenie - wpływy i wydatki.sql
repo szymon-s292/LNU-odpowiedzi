0589ae4a23d930e2a83b1ed066e2a5b4b2e7d015
Zeroks <85626218+Zeroks1@users.noreply.github.com>
2024-12-06 22:47:53 +0100

    Update 17. Ćwiczenie - wpływy i wydatki.sql
    poprawa błędu

--- SQL/Poziom Rozszerzony/Łączenie tabel cz. 2/17. Ćwiczenie - wpływy i wydatki.sql
+++ SQL/Poziom Rozszerzony/Łączenie tabel cz. 2/17. Ćwiczenie - wpływy i wydatki.sql
@@ -1,1 +1,1 @@
-SELECT "outgoings" AS field, SUM(amount) sum FROM account WHERE amount < 0;
+SELECT 'income' AS field, SUM(amount) AS sum FROM account WHERE amount > 0 UNION ALL SELECT 'outgoings', SUM(amount) FROM account WHERE amount < 0;
