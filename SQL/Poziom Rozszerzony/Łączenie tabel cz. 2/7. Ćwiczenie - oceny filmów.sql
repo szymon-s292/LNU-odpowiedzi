92db6fd93f1d4c7e585c3cff5eb4cd92d8b10af7
Zeroks <85626218+Zeroks1@users.noreply.github.com>
2024-12-06 22:39:41 +0100

    Update 7. Ćwiczenie - oceny filmów.sql

--- SQL/Poziom Rozszerzony/Łączenie tabel cz. 2/7. Ćwiczenie - oceny filmów.sql
+++ SQL/Poziom Rozszerzony/Łączenie tabel cz. 2/7. Ćwiczenie - oceny filmów.sql
@@ -1,1 +1,1 @@
-ERROR: RIGHT and FULL OUTER JOINs are not currently supported
+SELECT m.id, m.title, m.rating, mm.rating AS my_rating FROM movies m LEFT JOIN my_movies mm ON m.id = mm.id ORDER BY m.title;
